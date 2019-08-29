3852099f1e43a2baeb62d144646c3e5d6422da95
Logan McAnsh <logan@mcan.sh>
2019-08-28 21:01:01 -0400

    chore(deps): bump to latest
    add prettier config based on updated eslint-config
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['mcansh/typescript'],
-}
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['prettier.config.js'],
+      },
+    ],
+  },
+};
